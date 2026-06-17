1ad71a9d5b752bf683085153e03425a748ac9a72
tanay141096-del <tanay141096-del@users.noreply.github.com>
2026-06-17 07:53:00 +0000

    deploy: 7a45ad01f0745c125fde5cba1e30b42303920b36

---
+++ _next/static/9nKybhOB-PAskoQszU3-C/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/acsensia-v2-build//_next/:path+",
+        "destination": "/acsensia-v2-build/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
